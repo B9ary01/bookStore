e0ff696e53f5e4042c64ef4f9aa79ceb793cdf19
B9ary01 <bishalrai111@yahoo.com>
2022-10-08 00:31:59 +0100

    show stored books

--- routes/main.js
+++ routes/main.js
@@ -6,11 +6,11 @@ app.get('/addbook',function(req,res){ res.render('addbook.html');  });
 
 //mongo url
 var url="mongodb+srv://test123:test@cluster0.vef7a.mongodb.net/?retryWrites=true&w=majority"
+var MongoClient = require('mongodb').MongoClient;
 
 //bookadded route
 app.post('/bookadded', function (req,res) {
     // saving data in database
-        var MongoClient = require('mongodb').MongoClient;
             
          MongoClient.connect(url, async function(err, client) {
             if (err) throw err;
@@ -26,6 +26,20 @@ app.post('/bookadded', function (req,res) {
       res.send( '<a href='+'http://localhost:3000/'+'>Home</a>'+ 
       '<br />'+'This book is added to the database, name: '+ req.body.name + ' and price is : £'+ req.body.price);   
          });  }); 
+
+
+//display all the available books
+app.get('/booklist', function(req, res) {
+   
+              MongoClient.connect(url, async function (err, client) {
+                    if (err) throw err;
+           var db = await client.db('mybookshopdb');
+            await db.collection('books').find().toArray((findErr, results) => {
+                 if (findErr) throw findErr;
+               else
+           res.render('booklist.ejs', {availablebooks:results});
+           client.close();
+        }); });  });
 
 }
    
